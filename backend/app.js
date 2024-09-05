82fe2084f773d5e846463516fe9c8bf4469e409f
ophardik <116378316+ophardik@users.noreply.github.com>
2024-09-05 20:46:06 +0530

    Updated bugs

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,23 @@
+// import express from 'express'
+// import {userRouter} from './routes/User.js'
+// import cookieParser from "cookie-parser";
+// import cors from 'cors'
+// export const app=express();
+
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(cookieParser());
+// app.use(cors())
+
+
+// app.use("/api/v1",userRouter)
+
 import express from 'express';
 import { userRouter } from './routes/User.js'; // Ensure this path is correct
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-const app = express();
+export const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -24,3 +38,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
